28170683764be8908895d0045d5e85f76dd4c4ad
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-08-09 17:47:11 +0200

    Update NotFound.jsx

--- EnJoY-Final/src/components/NotFound.jsx
+++ EnJoY-Final/src/components/NotFound.jsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Img } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/Button";
@@ -8,6 +8,10 @@ export const NotFound = () => {
     <Flex flexDir="column" align="center" p="25%">
       <Heading>SORRY!</Heading>
       <p>The page doesnt exist!..</p>
+      <Img
+        src="https://cdn-icons-png.flaticon.com/512/1670/1670189.png"
+        alt="stop"
+      />
       <Link to={"/"}>
         <Button>Back to homepage...</Button>
       </Link>
